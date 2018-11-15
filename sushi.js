a4cf85f3f57c676d901f367b20027afc6e4dc49c
sjschrock <37678179+sjschrock@users.noreply.github.com>
2018-11-14 19:19:24 -0700

    Initial commit

---
+++ sushi.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var path = require('path');
+//var bootstrap = require('bootstrap');
+var app = express();
+var dir = path.join(__dirname, 'public');
+
+app.use(express.static(dir));
+
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 5169);
+
+app.get('/', function(req,res){
+  res.render('home');
+});
+
+app.get('/home', function(req,res){
+  res.render('home');
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://flip3.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
+
+
